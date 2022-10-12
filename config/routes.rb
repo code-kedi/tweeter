40a8d065ee36f90c866fdc26e97f75eef3b4c45c
Sonja Peschke <sonjape91@gmail.com>
2022-10-12 17:51:45 +0200

    Updated create method, created form & tweet partial, added deletion of form input (after creating a tweet)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :tweets, except: [:edit, :update]
 
+  resources :profiles
+
   devise_for :users
   root to: 'tweets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
