78fc1e7ec32ed2d27f8a941dab5b577f28912ba6
Sonja Peschke <sonjape91@gmail.com>
2022-10-20 13:17:26 +0200

    Created Like model, added associations to Tweet & Comment models, created like route and likes controller with create action

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,23 @@
+class LikesController < ApplicationController
+  before_action :set_likeable
+
+  def create
+    if @likeable.likes >= 1 && @likeable.liked_by?(current_user)
+      # if it's liked you want to be able to unlike it
+      @like = Like.find_by(likeable_id: @likeable.id, user: current_user)
+      @like.destroy
+    else
+      # when you like a tweet, you can only like it once
+      @like = @likeable.likes.new
+      @like.user = current_user
+      @like.save!
+    end
+  end
+
+  private
+
+  def set_likeable
+    @likeable = params[:likeable_type].constantize.find(params[:likeable_id])
+    # params[:likeable_type].constantize will turn into a class name, e.g. Tweet.find()
+  end
+end
